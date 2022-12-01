7fd6150a001280c6cb5a87de0fc9dc5ff935ae18
zayd699 <75783789+zayd699@users.noreply.github.com>
2022-12-02 00:49:01 +0100

    Add files via upload

---
+++ pfa_zayd_bouajaj/layers.py
@@ -0,0 +1,18 @@
+# Custom L1 Distance layer module 
+# WHY DO WE NEED THIS: its needed to load the custom model
+
+# Import dependencies
+import tensorflow as tf 
+from tensorflow.python.keras.engine.base_layer import Layer
+
+
+# Custom L1 Distance Layer from Jupyter
+class L1Dist(Layer):
+
+    # Init method - inheritance
+    def __init__(self, **kwargs):
+        super().__init__()
+
+    # Magic happens here - similarity calculation
+    def call(self, input_embedding, validation_embedding):
+        return tf.math.abs(input_embedding - validation_embedding)
